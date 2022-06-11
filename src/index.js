234b614067ce40c3e2d1c68b1bd33e09a54ed9ad
AnnaRabychina <nutochka@tut.by>
2022-06-11 23:51:00 +0300

    feat: implement program finish

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { getUserName } from '../services/getUserName.js';
 import { getHomeDir } from '../services/getHomeDir.js';
 
 const rl = readline.createInterface({
-  input: process.stdin
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter your command:\n'
 });
 
 export const runApp = async () => {
@@ -13,6 +15,19 @@ export const runApp = async () => {
   let currentDir = await getHomeDir();
   console.log(`You are currently in ${currentDir}`);
 
+  rl.prompt();
+
+  rl.on('line', (input) => {
+    const inputToString = input.trim().toLowerCase();
+    switch (inputToString) {
+      case '.exit':
+      case 'exit':
+        process.exit();
+      
+    }
+  }).on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}!`);
+    });
 }
 
 runApp();
